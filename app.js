c8a360ca1d3fe8b3333146324bd1fa392b41404a
zaid <zaid69232@gmail.com>
2024-09-06 12:06:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+// chap 8
+// assignment 2
+// question no 1
+// console.log("hello" +" "+ "world")
+// var firstname = "john"
+// var lastname = "vick"
+// var fullname = firstname + " " + lastname
+// console.log(fullname);
+// document.write("<h1>" + fullname + "</h1>")
+// console.log("2"+ "2")
+// console.log("2 plus 2 equals " + 2 + 2 );
+// chap 9
+// quesstion no 2
+var username = prompt("enter your name")
+console.log(username);
+alert(username)
+
+var num = +prompt("enter your name")
+console.log(num + 3);
+console.log(num + 2);
+console.log(num + 1);
+console.log(num + 1);
+console.log(num + 1);
+console.log(num + 1);
+console.log(num + 1);
+
+var username = prompt("enter your name","zaid")
+console.log(username)
+
+var username = prompt("enter your name","+924")
+console.log(phone)
+
+// ????
+var num1 = +prompt("enter your first number?")
+var opt = +prompt("enter your operator")
+var num2 = +prompt("enter your second number")
+
+console.log("num1 + opt + num2");
+
+
+
+
+
+
+
+
+
+
